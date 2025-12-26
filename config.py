5a12f847c4900d1f9ad435d060ea432da2a7d112
yrmamafat <102125669+yrmamafat@users.noreply.github.com>
2025-12-26 09:09:24 +0545

    Add configuration file for API keys and settings

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+amazon_access_key = 'YOUR_AMAZON_ACCESS_KEY'
+amazon_secret_key = 'YOUR_AMAZON_SECRET_KEY'
+amazon_associate_tag = 'YOUR_AMAZON_ASSOCIATE_TAG'
+
+openai_api_key = 'YOUR_OPENAI_API_KEY'
+
+youtube_api_key = 'YOUR_YOUTUBE_API_KEY'
+youtube_credentials_file = 'path_to_your_credentials.json'  # Path to the downloaded credentials.json file
